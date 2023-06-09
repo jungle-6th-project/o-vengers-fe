7f1ad6689d6983a1e9b231f1875246fc05dd9806
Seohyeon Lee <apple121626@gmail.com>
2023-06-09 23:23:28 +0900

    OVG-13 feat: ranking components for each user

--- src/components/RankingProfile.tsx
+++ src/components/RankingProfile.tsx
@@ -1,34 +1,82 @@
 import React from 'react';
-import { MyRankingProfileProps } from './MyRankingProfile';
 
-interface RankingProfileProps extends MyRankingProfileProps {
+interface RankingProfileProps {
+  username: string;
+  studyTime: string;
+  profileImg: string;
+  bgColor: string;
+  textColor: string;
+}
+
+interface OtherRankingProfileProps extends RankingProfileProps {
   rank: number;
 }
 
-const RankingProfile: React.FC<RankingProfileProps> = ({
-  rank,
+const RankingProfile: React.FC<RankingProfileProps & { title: string }> = ({
+  title,
   username,
   studyTime,
   profileImg,
+  bgColor,
+  textColor,
 }) => {
   return (
-    <div className="stats">
-      <div className="stat">
-        <div className="stat-title text-primary">{`${rank}위`}</div>
-        <div className="stat-figure">
-          <div className="avatar">
-            <div className="w-16 rounded-full">
-              <img src={profileImg} alt={username} />
-            </div>
-          </div>
+    <div className={`stat ${bgColor}`}>
+      <div className={`stat-title ${textColor}`}>{title}</div>
+      <div>
+        <div
+          className="stat-title text-sm w-28"
+          style={{
+            whiteSpace: 'nowrap',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            maxWidth: '100%',
+          }}
+        >
+          {username}
         </div>
-        <div>
-          <div className="stat-title text-sm">{username}</div>
-          <div className="stat-desc text-xs">{studyTime}</div>
+        <div className="stat-desc text-xs">{studyTime}</div>
+      </div>
+      <div className="stat-figure">
+        <div className="avatar">
+          <div className="w-16 mask mask-squircle">
+            <img src={profileImg} alt={username} />
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default RankingProfile;
+// TODO: sticky 속성 추가
+const MyRankingProfile: React.FC<
+  Omit<RankingProfileProps, 'bgColor' | 'textColor'>
+> = ({ username, studyTime, profileImg }) => {
+  return (
+    <RankingProfile
+      username={username}
+      studyTime={studyTime}
+      profileImg={profileImg}
+      title="Me"
+      bgColor="bg-primary"
+      textColor="text-secondary"
+    />
+  );
+};
+
+const OtherRankingProfile: React.FC<
+  Omit<OtherRankingProfileProps, 'bgColor' | 'textColor'>
+> = ({ rank, username, studyTime, profileImg }) => {
+  return (
+    <RankingProfile
+      username={username}
+      studyTime={studyTime}
+      profileImg={profileImg}
+      title={`${rank}위`}
+      bgColor=""
+      textColor="text-primary"
+    />
+  );
+};
+
+export { MyRankingProfile, OtherRankingProfile };
