35e3039b6e51b1b2d5361e518e05f66815e2113d
KINHYEONJI <hyeonji0718@gmail.com>
2023-06-16 20:47:26 +0900

    gut

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './index.css';
 import App from './App';
 import Login from './routes/Login';
 import KakaoCallback from './routes/KaKaoCallback';
+import RightBar from './components/RightBar/RightBar';
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
   {
     path: '/kakao_redirect',
     element: <KakaoCallback />,
+  },
+  {
+    path: '/study/:roomID',
+    element: <RightBar />,
   },
 ]);
 
