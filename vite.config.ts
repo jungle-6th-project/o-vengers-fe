f18cd43ef77104dda9f7fcc43be24a51c5a33fd3
KINHYEONJI <hyeonji0718@gmail.com>
2023-06-18 00:01:25 +0900

    chore : 절대 경로 사용을 위한 package 다운로드

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     proxy: {
       '/socket.io/': {
-        target: 'ws://localhost:3000',
+        target: 'ws://localhost:5000',
         ws: true,
         changeOrigin: true,
         secure: false,
