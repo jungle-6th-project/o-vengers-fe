45698ba54ae45b176be02771ca3b97ee67781d24
KINHYEONJI <hyeonji0718@gmail.com>
2023-06-18 15:45:54 +0900

    feat(GroupJoin) : 이미 참여한 그룹일 경우 예외처리

--- src/components/GroupJoinModal.tsx
+++ src/components/GroupJoinModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useCookies } from 'react-cookie';
@@ -8,9 +8,16 @@ interface GroupJoinModalProps {
   joinPath: string;
 }
 
+interface ErrorResponse {
+  response?: {
+    status: number;
+  };
+}
+
 const GroupJoinModal = ({ joinPath }: GroupJoinModalProps) => {
   const joinModalRef = useRef<HTMLDialogElement>(null);
   const [{ accessToken }] = useCookies(['accessToken']);
+  const [isAlreadyJoined, setAlreadyJoined] = useState(false);
   const navigate = useNavigate();
 
   const { data } = useQuery(['getGroupName'], () =>
@@ -18,14 +25,15 @@ const GroupJoinModal = ({ joinPath }: GroupJoinModalProps) => {
   );
 
   const postPathJoinGroupMutation = useMutation(
-    (values: { accessToken: string; path: string }) => pathJoinGroup(values)
+    (values: { accessToken: string; path: string }) => pathJoinGroup(values),
+    {
+      onError: (error: ErrorResponse) => {
+        if (error.response?.status === 400) {
+          setAlreadyJoined(true);
+        }
+      },
+    }
   );
-
-  const handleModalClose = () => {
-    joinModalRef.current?.close();
-    postPathJoinGroupMutation.mutate({ accessToken, path: joinPath });
-    navigate('/');
-  };
 
   useEffect(() => {
     if (joinModalRef.current) {
@@ -33,23 +41,54 @@ const GroupJoinModal = ({ joinPath }: GroupJoinModalProps) => {
     }
   }, []);
 
+  const handleModalClose = () => {
+    joinModalRef.current?.close();
+    navigate('/');
+  };
+
+  const handleAcceptInvite = () => {
+    postPathJoinGroupMutation.mutate({ accessToken, path: joinPath });
+  };
+
+  const renderContent = () => {
+    if (isAlreadyJoined) {
+      return (
+        <div>
+          <p className="py-4">이미 가입된 그룹입니다!</p>
+          <div className="modal-action">
+            <button type="button" className="btn" onClick={handleModalClose}>
+              닫기
+            </button>
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <p className="py-4">{data?.groupName} 그룹에 초대되었습니다!</p>
+        <div className="modal-action">
+          <button type="button" className="btn" onClick={handleModalClose}>
+            취소
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={handleAcceptInvite}
+            disabled={postPathJoinGroupMutation.isLoading}
+          >
+            {postPathJoinGroupMutation.isLoading ? '로딩 중...' : '초대 수락'}
+          </button>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <dialog ref={joinModalRef} id="groupJoinModal" className="modal">
       <form method="dialog" className="w-11/12 max-w-5xl modal-box">
         <h3 className="text-lg font-bold">그룹 참여 요청</h3>
-        <p className="py-4">{data?.groupName} 그룹에 초대되었습니다!</p>
-        <div className="modal-action">
-          <button
-            type="button"
-            className="btn"
-            onClick={() => joinModalRef.current?.close()}
-          >
-            취소
-          </button>
-          <button type="button" className="btn" onClick={handleModalClose}>
-            초대 수락
-          </button>
-        </div>
+        {renderContent()}
       </form>
     </dialog>
   );
