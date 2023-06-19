0b4210dc43511fd996128d1a24a8be1fd5b7a001
KINHYEONJI <hyeonji0718@gmail.com>
2023-06-19 14:38:24 +0900

    feat(GroupJoin) : 잘못된 접근 예외처리 완료

--- src/components/GroupJoinModal.tsx
+++ src/components/GroupJoinModal.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { isValidElement, useEffect, useRef, useState } from 'react';
+import { redirect, useNavigate } from 'react-router-dom';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useCookies } from 'react-cookie';
 import { getGroupNameByPath, pathJoinGroup } from '../utils/api';
@@ -15,14 +15,13 @@ const GroupJoinModal = ({ joinPath }: GroupJoinModalProps) => {
   const [isNotValidPath, setIsNotValidPath] = useState(false);
   const navigate = useNavigate();
 
-  const { data, isLoading } = useQuery(['groupName'], async () => {
+  const { data } = useQuery(['groupName'], async () => {
     try {
       const groupData = await getGroupNameByPath({
         accessToken,
         path: joinPath,
       });
       return groupData;
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       console.log(error);
       if (
@@ -50,66 +49,56 @@ const GroupJoinModal = ({ joinPath }: GroupJoinModalProps) => {
 
   const handleModalClose = () => {
     joinModalRef.current?.close();
-    setTimeout(() => {
-      navigate('/');
-    }, 200);
+    navigate('/');
   };
 
   const handleAcceptInvite = () => {
     postPathJoinGroupMutation.mutate({ accessToken, path: joinPath });
-  };
-
-  const renderContent = () => {
-    if (isAlreadyJoined) {
-      return (
-        <div>
-          <p className="py-4">이미 가입된 그룹입니다!</p>
-          <div className="modal-action">
-            <button type="button" className="btn" onClick={handleModalClose}>
-              닫기
-            </button>
-          </div>
-        </div>
-      );
-    }
-
-    if (isNotValidPath) {
-      return (
-        <div>
-          <p className="py-4">유효하지 않은 초대코드 입니다.</p>
-          <div className="modal-action">
-            <button type="button" className="btn" onClick={handleModalClose}>
-              닫기
-            </button>
-          </div>
-        </div>
-      );
-    }
-
-    return (
-      <div>
-        <p className="py-4">{data?.groupName} 그룹에 초대되었습니다!</p>
-        <div className="modal-action">
-          <button type="button" className="btn" onClick={handleModalClose}>
-            취소
-          </button>
-          <button
-            type="button"
-            className="btn"
-            onClick={handleAcceptInvite}
-            disabled={postPathJoinGroupMutation.isLoading}
-          >
-            {postPathJoinGroupMutation.isLoading ? '로딩 중...' : '초대 수락'}
-          </button>
-        </div>
-      </div>
-    );
+    joinModalRef.current?.close();
+    navigate('/');
+    window.location.reload();
   };
 
   return (
     <dialog ref={joinModalRef} id="groupJoinModal" className="modal">
       <form method="dialog" className="w-11/12 max-w-5xl modal-box">
-        {!isLoading && renderContent()}
+        {isAlreadyJoined && (
+          <div>
+            <p className="py-4">이미 가입된 그룹입니다!</p>
+            <div className="modal-action">
+              <button type="button" className="btn" onClick={handleModalClose}>
+                닫기
+              </button>
+            </div>
+          </div>
+        )}
+        {isNotValidPath && (
+          <div>
+            <p className="py-4">유효하지 않은 초대코드 입니다.</p>
+            <div className="modal-action">
+              <button type="button" className="btn" onClick={handleModalClose}>
+                닫기
+              </button>
+            </div>
+          </div>
+        )}
+        {!isAlreadyJoined && !isNotValidPath && (
+          <div>
+            <p className="py-4">{data?.groupName} 그룹에 초대되었습니다!</p>
+            <div className="modal-action">
+              <button type="button" className="btn" onClick={handleModalClose}>
+                취소
+              </button>
+              <button
+                type="button"
+                className="btn"
+                onClick={handleAcceptInvite}
+              >
+                초대수락
+              </button>
+            </div>
+          </div>
+        )}
       </form>
     </dialog>
   );
