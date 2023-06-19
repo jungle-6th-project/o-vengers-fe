8af5e8b2bea15350408a56b0359bb530f681351e
KINHYEONJI <hyeonji0718@gmail.com>
2023-06-20 00:29:40 +0900

    feat(Todo) : Todo 생성 id 에러 처리

--- src/components/Todo/GroupTodo.tsx
+++ src/components/Todo/GroupTodo.tsx
@@ -20,24 +20,30 @@ const useKeyPress = (
     (values: { content: string; groupId: number }) => postTodo(values)
   );
 
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleKeyPress = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       e.preventDefault();
       const inputValue = e.currentTarget.value.trim();
       if (inputValue !== '') {
-        const newTodo: Todo = {
-          todoId: Math.random(),
-          groupId: groupData.groupId,
-          content: inputValue,
-          done: false,
-        };
-        postTodoMutation.mutate({
-          content: inputValue,
-          groupId: groupData.groupId,
-        });
-        setTodos(prevTodos => [newTodo, ...prevTodos]);
-        setInputValue('');
-        setShowInput(false);
+        try {
+          const data = await postTodoMutation.mutateAsync({
+            content: inputValue,
+            groupId: groupData.groupId,
+          });
+
+          const newTodo: Todo = {
+            todoId: data.todoId,
+            groupId: groupData.groupId,
+            content: inputValue,
+            done: false,
+          };
+          console.log(newTodo);
+          setTodos(prevTodos => [newTodo, ...prevTodos]);
+          setInputValue('');
+          setShowInput(false);
+        } catch (error) {
+          // handle error
+        }
       }
     }
   };
