a197385ee1b095e03a8829df03a17fa3455c007d
Seohyeon Lee <apple121626@gmail.com>
2023-06-22 22:32:33 +0900

    OVG-137 feat: splitted component, room expire exit

---
+++ src/components/RoomTimer/RoomExitModal.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { leaveVideoRoom } from '@/utils/api';
+
+declare global {
+  interface Window {
+    roomExitModal: HTMLDialogElement;
+  }
+}
+
+const ExitModal = ({ roomId }: { roomId: number }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleOnLeave = async () => {
+      navigate('/');
+      await leaveVideoRoom(roomId);
+    };
+
+    window.roomExitModal.showModal();
+    handleOnLeave();
+  }, [navigate, roomId]);
+
+  return (
+    <dialog id="roomExitModal" className="modal">
+      <form method="dialog" className="modal-box">
+        <h3 className="font-bold text-lg">수고하셨습니다!</h3>
+      </form>
+    </dialog>
+  );
+};
+
+export default ExitModal;
