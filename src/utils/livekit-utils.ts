f59375a0ede89503912d1d125ce0eba69bb7828f
minseok1109 <park485201@naver.com>
2023-06-26 04:26:05 +0900

    feat: change video chat url in produdction

--- src/utils/livekit-utils.ts
+++ src/utils/livekit-utils.ts
@@ -3,14 +3,16 @@ import { useEffect, useState } from 'react';
 function useServerUrl(region?: string) {
   const [serverUrl, setServerUrl] = useState<string | undefined>();
   useEffect(() => {
-    let endpoint = `/video/api/url`;
+    let endpoint =
+      import.meta.env.MODE === 'development '
+        ? `/video/api/url`
+        : `https://www.sangyeop.shop/video/api/url`;
     if (region) {
       endpoint += `?region=${region}`;
     }
     fetch(endpoint).then(async res => {
       if (res.ok) {
         const body = await res.json();
-        console.log('🚀 ~ file: livekit-utils.ts:13 ~ fetch ~ body:', body);
         setServerUrl(body.url);
       } else {
         throw Error('Error fetching server url, check server logs');
