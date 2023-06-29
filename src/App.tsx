7c182c81be54cf6d4682c52250d3dc43104fde63
minseok1109 <park485201@naver.com>
2023-06-29 12:13:23 +0900

    delete footer

--- src/App.tsx
+++ src/App.tsx
@@ -18,45 +18,32 @@ function App() {
 
   axios.defaults.headers.common.Authorization = `Bearer ${token.accessToken}`;
   return (
-    <>
-      <div className="h-screen p-10 grid gap-x-5 gap-y-5 grid-rows-container grid-cols-container w-max-full h-max-screen">
-        <div className="row-span-2 grid grid-rows-leftbar gap-3">
-          <div className="mb-8 w-ranking_todo min-w-leftbar max-w-leftbar h-[90px]">
-            <Logo width="100%" height="100%" />
-          </div>
-          <Ranking />
-          <TodoList />
+    <div className="grid h-screen p-10 gap-x-5 gap-y-5 grid-rows-container grid-cols-container w-max-full h-max-screen">
+      <div className="grid row-span-2 gap-3 grid-rows-leftbar">
+        <div className="mb-8 w-ranking_todo min-w-leftbar max-w-leftbar h-[90px]">
+          <Logo width="100%" height="100%" />
         </div>
-        <div className="Navigator max-w-calendar grid grid-cols-navigator gap-3">
-          <div className="flex flex-col justify-between mr-3 btn-3 h-groupList min-h-header-min max-h-header-max">
-            {isGroupPath && <GroupJoinModal joinPath={location[1]} />}
-            <GroupMakeModal />
-            <GroupSearchModal />
-            <Link to="/mypage">
-              <button type="button" className="btn btn-square">
-                MY
-              </button>
-            </Link>
-          </div>
-          <GroupsList />
-          <Timer />
-        </div>
-        <div className="h-full overflow-auto col-start-2 row-start-2 max-w-calendar">
-          <Calendar />
-        </div>
+        <Ranking />
+        <TodoList />
       </div>
-      <footer className="p-10 footer footer-center bg-primary text-primary-content">
-        <div>
-          <p className="font-bold">
-            리더스 푸르넷 공부방 - 뽀독뽀독 독서실 <br />
-          </p>
-          <p>
-            주소 : 광주광역시 광산구 도산동 남동길 30번길 13 | 사업자 등록 번호
-            : 865-91-01383 | 대표 번호 : 010-3649-3666 (원장)
-          </p>
+      <div className="grid gap-3 Navigator max-w-calendar grid-cols-navigator">
+        <div className="flex flex-col justify-between mr-3 btn-3 h-groupList min-h-header-min max-h-header-max">
+          {isGroupPath && <GroupJoinModal joinPath={location[1]} />}
+          <GroupMakeModal />
+          <GroupSearchModal />
+          <Link to="/mypage">
+            <button type="button" className="btn btn-square">
+              MY
+            </button>
+          </Link>
         </div>
-      </footer>
-    </>
+        <GroupsList />
+        <Timer />
+      </div>
+      <div className="h-full col-start-2 row-start-2 overflow-auto max-w-calendar">
+        <Calendar />
+      </div>
+    </div>
   );
 }
 
