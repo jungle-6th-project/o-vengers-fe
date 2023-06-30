664b88c5bdee2b89436f62ceba98f0f49cc232f3
Seohyeon Lee <apple121626@gmail.com>
2023-07-01 00:32:44 +0900

    OVG-193 feat: visible alarm before 1min

--- src/components/Timer/EntryButton.tsx
+++ src/components/Timer/EntryButton.tsx
@@ -1,15 +1,21 @@
+import './entryButton.css';
+
 const EntryButton = ({
   onIdle,
+  isNear,
   handleEnterRoom,
 }: {
   onIdle: boolean;
+  isNear: boolean;
   handleEnterRoom: () => void;
 }) => (
   <button
     type="button"
     className={`btn btn-outline ${
       onIdle ? '' : 'btn-accent'
-    } w-[14.125rem] h-[2.8125rem] text-2xl rounded-xl disabled:btn-ghost disabled:border-white disabled:text-white disabled:opacity-70`}
+    } w-[14.125rem] h-[2.8125rem] text-2xl rounded-xl disabled:btn-ghost disabled:border-white disabled:text-white disabled:opacity-70 ${
+      isNear && 'ripple'
+    }`}
     disabled={onIdle}
     onClick={handleEnterRoom}
   >
