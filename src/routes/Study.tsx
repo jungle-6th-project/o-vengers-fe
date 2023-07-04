684186a384f3f69386da32d3ca0f1ea4540a7d7b
minseok1109 <park485201@naver.com>
2023-07-05 00:09:00 +0900

    fix: 예약하지 않은 스터디룸 진입금지

--- src/routes/Study.tsx
+++ src/routes/Study.tsx
@@ -1,12 +1,45 @@
+import { QueryClient } from '@tanstack/react-query';
 import '@livekit/components-styles';
 import '@/components/Video/activeRoom.css';
 import { LocalUserChoices } from '@livekit/components-react';
 import { useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, redirect } from 'react-router-dom';
 import { useUser } from '@/store/userStore';
 import ActiveRoom from '@/components/Video/ActiveRoom';
-import { leaveVideoRoom } from '@/utils/api';
+import { getUserNearestReservation, leaveVideoRoom } from '@/utils/api';
 import { PreJoin } from '@/components/Video/Prejoin';
+
+interface NearestReservationData {
+  endTime: string;
+  groupId: number;
+  profiles: string[];
+  roomId: number;
+  startTime: string;
+}
+
+const nearestReservationQuery = () => ({
+  queryKey: ['userNearestReservation'],
+  queryFn: () => getUserNearestReservation(),
+});
+
+export const loader =
+  (queryClient: QueryClient) =>
+  async ({ request }: { request: Request }) => {
+    const url = new URL(request.url);
+    const urlRoomId = url.pathname.split('/')[2];
+    if (!queryClient.getQueryData(nearestReservationQuery().queryKey)) {
+      await queryClient.fetchQuery(nearestReservationQuery());
+    }
+
+    const nearestReservationData: NearestReservationData | undefined =
+      queryClient.getQueryData(nearestReservationQuery().queryKey);
+
+    if (Number(urlRoomId) !== nearestReservationData?.roomId) {
+      return redirect('/');
+    }
+
+    return null;
+  };
 
 function App() {
   const { roomId } = useParams();
